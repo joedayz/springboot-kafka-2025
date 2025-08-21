f6deef3d6216385dda192ad9e31c54523f40734a
Jose Diaz <jose.diaz@joedayz.pe>
2025-08-20 20:31:03 -0500

    mock service

---
+++ mockservice/src/test/java/pe/joedayz/ws/mockservice/MockserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.joedayz.ws.mockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
