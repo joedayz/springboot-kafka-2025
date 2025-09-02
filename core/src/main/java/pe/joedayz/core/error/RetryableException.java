8309753f3b2079d8aa28f3fd4477a4b752d0084e
Jose Diaz <jose.diaz@joedayz.pe>
2025-09-01 20:08:54 -0500

    new events

---
+++ core/src/main/java/pe/joedayz/core/error/RetryableException.java
@@ -0,0 +1,11 @@
+package pe.joedayz.core.error;
+
+public class RetryableException extends RuntimeException {
+    public RetryableException(Throwable cause) {
+        super(cause);
+    }
+
+    public RetryableException(String message) {
+        super(message);
+    }
+}
